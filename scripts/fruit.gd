851534673af12aa3b512d99b3c6efde599879fdc
Mohammed Aslam P. A <modaslam1327@gmail.com>
2020-12-26 20:07:34 +0530

    splitting a fruit

--- scripts/fruit.gd
+++ scripts/fruit.gd
@@ -8,7 +8,10 @@ onready var body2 = get_node("Body2")
 onready var sprite1 = get_node("Body1/Sprite1")
 onready var sprite2 = body2.get_node("Sprite2")
 
-var cut = false
+var didCut = false
+
+signal score
+signal life
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -26,6 +29,30 @@ func generate(initialPos):
 	linear_velocity = initialVel
 	angular_velocity = rand_range(-10, 10)
 
+
+func cut():
+	if didCut: return
+	didCut = true
+	emit_signal("score")
+	mode = MODE_KINEMATIC
+	sprite0.queue_free()
+	shape.queue_free()
+	body1.mode = MODE_RIGID
+	body2.mode = MODE_RIGID
+	body1.apply_impulse(Vector2(0, 0), Vector2(-100, 0).rotated(rotation))
+	body2.apply_impulse(Vector2(0, 0), Vector2(100, 0).rotated(rotation))
+	body1.angular_velocity = angular_velocity
+	body2.angular_velocity = angular_velocity
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	if position.y > 800:
+		print("Reached ground")
+		emit_signal("life")
+		queue_free()
+	if body1.position.y > 800 and body2.position.y > 800:
+		queue_free()
+
+
+func _on_Timer_timeout():
+	cut()
